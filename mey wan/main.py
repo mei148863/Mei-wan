83e6165c1f49a4c16bc40c1e26be36fac1ea4204
mei148863 <meywanmey67@gmail.com>
2025-01-06 10:51:52 +0800

    Add files via upload

---
+++ mey wan/main.py
@@ -0,0 +1,10 @@
+import math
+
+# input Masukan nilai jari-jari
+radius = float(input("masukan jari-jari linkaran: " ))
+
+# hitung luas lingkaran
+area = math.pi* radius **2
+
+#Menampilkan hasil
+print(f"Luas lingkaran dengan jari-jari {radius} adalah {area}")
